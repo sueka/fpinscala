f720de1df598a70ffde5af1e3c509b2fa36ee1cd
sueka <mail@sueka.me>
2017-11-03 23:12:42 +0900

    :dromedary_camel: solve exercise 3.4

--- exercises/src/main/scala/fpinscala/datastructures/List.scala
+++ exercises/src/main/scala/fpinscala/datastructures/List.scala
@@ -61,7 +61,14 @@ object List { // `List` companion object. Contains functions for creating and wo
     case Nil => Nil
   }
 
-  def drop[A](l: List[A], n: Int): List[A] = ???
+  // Exercise 4
+  @annotation.tailrec
+  def drop[A](l: List[A], n: Int): List[A] =
+    if (n == 0) l
+    else l match {
+      case Cons(_, t) => drop(t, n - 1)
+      case Nil => null
+    }
 
   def dropWhile[A](l: List[A], f: A => Boolean): List[A] = ???
 
