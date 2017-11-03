a549bce473865339447e6d0c073bc483c8928dda
sueka <mail@sueka.me>
2017-11-03 23:07:24 +0900

    :dromedary_camel: solve exercise 3.2

--- exercises/src/main/scala/fpinscala/datastructures/List.scala
+++ exercises/src/main/scala/fpinscala/datastructures/List.scala
@@ -49,8 +49,11 @@ object List { // `List` companion object. Contains functions for creating and wo
   def product2(ns: List[Double]) =
     foldRight(ns, 1.0)(_ * _) // `_ * _` is more concise notation for `(x,y) => x * y`; see sidebar
 
-
-  def tail[A](l: List[A]): List[A] = ???
+  // Exercise 2
+  def tail[A](l: List[A]): List[A] = l match {
+    case Cons(_, t) => t
+    case Nil => null
+  }
 
   def setHead[A](l: List[A], h: A): List[A] = ???
 
