eaf5abcbd30ed23fca74cc4fdaae417c7503fa99
sueka <mail@sueka.me>
2017-11-04 14:26:39 +0900

    :dromedary_camel: solve exercise 3.10

--- exercises/src/main/scala/fpinscala/datastructures/List.scala
+++ exercises/src/main/scala/fpinscala/datastructures/List.scala
@@ -87,7 +87,12 @@ object List { // `List` companion object. Contains functions for creating and wo
   // Exercise 9
   def length[A](l: List[A]): Int = foldRight(l, 0)((_, res) => res + 1)
 
-  def foldLeft[A,B](l: List[A], z: B)(f: (B, A) => B): B = ???
+  // Exercise 10
+  @annotation.tailrec
+  def foldLeft[A,B](l: List[A], z: B)(f: (B, A) => B): B = l match {
+    case Cons(h, t) => foldLeft(t, f(z, h))(f)
+    case Nil => z
+  }
 
   def map[A,B](l: List[A])(f: A => B): List[B] = ???
 }
