6107d2145bf1b7edb9f83e9d2228a70da976133c
sueka <mail@sueka.me>
2017-11-05 12:00:49 +0900

    :dromedary_camel: solve exercise 3.25

--- exercises/src/main/scala/fpinscala/datastructures/Tree.scala
+++ exercises/src/main/scala/fpinscala/datastructures/Tree.scala
@@ -6,6 +6,11 @@ case class Branch[A](left: Tree[A], right: Tree[A]) extends Tree[A]
 
 
 object Tree {
+  // Exercise 25
+  def size[A](tree: Tree[A]): Int = tree match {
+    case Leaf(_) => 1
+    case Branch(l, r) => size(l) + size(r)
+  }
 
 
 
