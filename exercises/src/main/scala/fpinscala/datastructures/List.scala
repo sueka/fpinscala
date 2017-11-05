1211714ef56eba37589eea4dc815937d8b32dd00
sueka <mail@sueka.me>
2017-11-05 11:32:14 +0900

    :dromedary_camel: solve exercise 3.20

--- exercises/src/main/scala/fpinscala/datastructures/List.scala
+++ exercises/src/main/scala/fpinscala/datastructures/List.scala
@@ -143,4 +143,7 @@ object List { // `List` companion object. Contains functions for creating and wo
   // Exercise 19
   def filter[A](as: List[A])(f: A => Boolean): List[A] =
     foldRight(as, Nil: List[A])((h, t) => if (f(h)) Cons(h, t) else t)
+
+  // Exercise 20
+  def flatMap[A,B](as: List[A])(f: A => List[B]): List[B] = flatten(map(as)(f))
 }
